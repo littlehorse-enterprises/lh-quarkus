c67d623760f27f556bdea8ca094c4e632f4cd300
Saúl Piña <sauljabin@gmail.com>
2025-05-27 17:30:17 -0500

    refactor: remove unused annotation

--- runtime/src/main/java/io/littlehorse/quarkus/runtime/recordable/LHRecordable.java
+++ runtime/src/main/java/io/littlehorse/quarkus/runtime/recordable/LHRecordable.java
@@ -1,14 +1,12 @@
 package io.littlehorse.quarkus.runtime.recordable;
 
-import io.quarkus.runtime.annotations.RecordableConstructor;
-
 import jakarta.enterprise.inject.spi.CDI;
 
 public abstract class LHRecordable {
+
     private final Class<?> beanClass;
     private final String name;
 
-    @RecordableConstructor
     public LHRecordable(Class<?> beanClass, String name) {
         this.beanClass = beanClass;
         this.name = name;
