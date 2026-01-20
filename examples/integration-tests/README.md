50c8837d4ca0d2bb2b745ccee50ca7e849d8ef7b
Saúl Piña <sauljabin@gmail.com>
2026-01-20 12:18:44 -0500

    feat: add openapi

--- examples/integration-tests/README.md
+++ examples/integration-tests/README.md
@@ -19,11 +19,13 @@ Execute native tests:
 ## Troubleshooting
 
 In case you receive this error:
-```shell
+
+```
 BadRequestException (Status 400: {"message":"client version 1.32 is too old. Minimum supported API version is 1.44, please upgrade your client to a newer version"})
 ```
 
 Run this command:
+
 ```shell
 echo api.version=1.44 >> ~/.docker-java.properties
 ```
--- extensions/littlehorse-quarkus-proxy/runtime/src/main/java/io/littlehorse/quarkus/proxy/resource/wfspec/WfSpecResource.java
+++ extensions/littlehorse-quarkus-proxy/runtime/src/main/java/io/littlehorse/quarkus/proxy/resource/wfspec/WfSpecResource.java
@@ -6,6 +6,13 @@ import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/proxy/tenants/{tenant}/wf-specs")
 public class WfSpecResource {
@@ -18,6 +25,85 @@ public class WfSpecResource {
 
     @GET
     @Path("/{wfSpecId}")
+    @APIResponses({
+        @APIResponse(
+                responseCode = "200",
+                description = "WfSpec retrieved successfully",
+                content =
+                        @Content(
+                                mediaType = MediaType.APPLICATION_JSON,
+                                schema =
+                                        @Schema(
+                                                examples = {"""
+                                    {
+                                      "id": {
+                                        "name": "greetings",
+                                        "majorVersion": 0,
+                                        "revision": 0
+                                      },
+                                      "createdAt": "2026-01-20T16:10:06.917Z",
+                                      "frozenVariables": [],
+                                      "status": "ACTIVE",
+                                      "threadSpecs": {
+                                        "entrypoint": {
+                                          "nodes": {
+                                            "0-entrypoint-ENTRYPOINT": {
+                                              "outgoingEdges": [{
+                                                "sinkNodeName": "1-greetings-TASK",
+                                                "variableMutations": []
+                                              }],
+                                              "failureHandlers": [],
+                                              "entrypoint": {
+                                              }
+                                            },
+                                            "1-greetings-TASK": {
+                                              "outgoingEdges": [{
+                                                "sinkNodeName": "2-exit-EXIT",
+                                                "variableMutations": []
+                                              }],
+                                              "failureHandlers": [],
+                                              "task": {
+                                                "taskDefId": {
+                                                  "name": "greetings"
+                                                },
+                                                "timeoutSeconds": 60,
+                                                "retries": 0,
+                                                "variables": [{
+                                                  "variableName": "name"
+                                                }]
+                                              }
+                                            },
+                                            "2-exit-EXIT": {
+                                              "outgoingEdges": [],
+                                              "failureHandlers": [],
+                                              "exit": {
+                                              }
+                                            }
+                                          },
+                                          "variableDefs": [{
+                                            "varDef": {
+                                              "name": "name",
+                                              "typeDef": {
+                                                "primitiveType": "STR",
+                                                "masked": false
+                                              }
+                                            },
+                                            "required": false,
+                                            "searchable": false,
+                                            "jsonIndexes": [],
+                                            "accessLevel": "PRIVATE_VAR"
+                                          }],
+                                          "interruptDefs": []
+                                        }
+                                      },
+                                      "entrypointThreadName": "entrypoint"
+                                    }
+                                    """},
+                                                externalDocs =
+                                                        @ExternalDocumentation(
+                                                                url =
+                                                                        "https://littlehorse.io/docs/server/api#wfspec"))))
+    })
     public Uni<WfSpec> get(@PathParam("wfSpecId") String wfSpecId) {
         return service.get(wfSpecId);
     }
