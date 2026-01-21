19e090d7b376ded79cf51be37d504555b352bdbb
Saúl Piña <sauljabin@gmail.com>
2026-01-20 19:54:15 -0500

    feat: search endpoint and openapi documentation

--- extensions/littlehorse-quarkus-proxy/runtime/src/main/java/io/littlehorse/quarkus/proxy/resource/wfspec/WfSpecResource.java
+++ extensions/littlehorse-quarkus-proxy/runtime/src/main/java/io/littlehorse/quarkus/proxy/resource/wfspec/WfSpecResource.java
@@ -1,16 +1,24 @@
 package io.littlehorse.quarkus.proxy.resource.wfspec;
 
 import io.littlehorse.sdk.common.proto.WfSpec;
+import io.littlehorse.sdk.common.proto.WfSpecIdList;
 import io.smallrye.mutiny.Uni;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
@@ -28,7 +36,7 @@ public class WfSpecResource {
     @APIResponses({
         @APIResponse(
                 responseCode = "200",
-                description = "WfSpec retrieved successfully",
+                description = "Record retrieved successfully",
                 content =
                         @Content(
                                 mediaType = MediaType.APPLICATION_JSON,
@@ -104,7 +112,53 @@ public class WfSpecResource {
                                                                 url =
                                                                         "https://littlehorse.io/docs/server/api#wfspec"))))
     })
+    @Parameters(
+            value = {@Parameter(name = "tenant", in = ParameterIn.PATH, description = "Tenant name")
+            })
     public Uni<WfSpec> get(@PathParam("wfSpecId") String wfSpecId) {
         return service.get(wfSpecId);
+    }
+
+    @GET
+    @APIResponses({
+        @APIResponse(
+                responseCode = "200",
+                description = "List retrieved successfully",
+                content =
+                        @Content(
+                                mediaType = MediaType.APPLICATION_JSON,
+                                schema =
+                                        @Schema(
+                                                examples = {"""
+                                            {
+                                              "results": [{
+                                                "name": "greetings",
+                                                "majorVersion": 0,
+                                                "revision": 0
+                                              }],
+                                              "bookmark": "ChgIABIUEhIyL3dmLTAvMDAwMDAvMDAwMDA="
+                                            }
+                                    """},
+                                                externalDocs =
+                                                        @ExternalDocumentation(
+                                                                url =
+                                                                        "https://littlehorse.io/docs/server/api#wfspecidlist"))))
+    })
+    @Parameters(
+            value = {@Parameter(name = "tenant", in = ParameterIn.PATH, description = "Tenant name")
+            })
+    public Uni<WfSpecIdList> search(
+            @Parameter(description = "Maximum number of workflow specs to return")
+                    @QueryParam("limit")
+                    @Positive
+                    @Max(100)
+                    @DefaultValue("10")
+                    Integer limit,
+            @Parameter(description = "Search prefix for workflow names") @QueryParam("prefix")
+                    String prefix,
+            @Parameter(description = "Bookmark for pagination, it is a base64 string")
+                    @QueryParam("bookmark")
+                    String bookmark) {
+        return service.search(prefix, bookmark, limit);
     }
 }
