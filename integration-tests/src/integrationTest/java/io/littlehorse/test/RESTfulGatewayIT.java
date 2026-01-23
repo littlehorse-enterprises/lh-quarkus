e522a9d431994afaa5e145e9cb6a41941eb4918f
Saúl Piña <sauljabin@gmail.com>
2026-01-23 18:13:44 -0500

    test: add int tests for searh wf spec

--- integration-tests/src/integrationTest/java/io/littlehorse/test/RESTfulGatewayIT.java
+++ integration-tests/src/integrationTest/java/io/littlehorse/test/RESTfulGatewayIT.java
@@ -2,11 +2,15 @@ package io.littlehorse.test;
 
 import static io.restassured.RestAssured.given;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 import io.littlehorse.common.ContainersTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +42,52 @@ class RESTfulGatewayIT {
                 .statusCode(200)
                 .body("id.name", is(wfSpecName))
                 .body("status", is("ACTIVE"))
+                .log()
+                .all();
+    }
+
+    @Test
+    void shouldSearchAllWfSpec() {
+        given().pathParam("tenant", "default")
+                .when()
+                .get("/gateway/tenants/{tenant}/wf-specs")
+                .then()
+                .statusCode(200)
+                .body("results", hasSize(2))
+                .body("bookmark", is(nullValue()))
+                .body("results[0].name", is("greetings"))
+                .body("results[1].name", is("json"))
+                .log()
+                .all();
+    }
+
+    @Test
+    void shouldSearchWfSpecWithBookmark() {
+        Response getFirstObject = given().pathParam("tenant", "default")
+                .queryParam("limit", 1)
+                .when()
+                .get("/gateway/tenants/{tenant}/wf-specs");
+        String bookmark = getFirstObject.jsonPath().getString("bookmark");
+
+        getFirstObject
+                .then()
+                .statusCode(200)
+                .body("results", hasSize(1))
+                .body("results[0].name", is("greetings"))
+                .body("bookmark", is(notNullValue()))
+                .log()
+                .all();
+
+        given().pathParam("tenant", "default")
+                .queryParam("limit", 1)
+                .queryParam("bookmark", bookmark)
+                .when()
+                .get("/gateway/tenants/{tenant}/wf-specs")
+                .then()
+                .statusCode(200)
+                .body("results", hasSize(1))
+                .body("bookmark", is(nullValue()))
+                .body("results[0].name", is("json"))
                 .log()
                 .all();
     }
